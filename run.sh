d73708b3dbd19531ef876ba07224ca39b8105ba5
Michael Sacchi <michael.sacchi@ppro.com>
2018-11-29 13:02:08 +0100

    Configurable UID/GID

--- run.sh
+++ run.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
 mkdir -p $MOUNTPOINT
-s3fs $BUCKET $MOUNTPOINT &&  echo $BUCKET mounted on $MOUNTPOINT...
-trap : TERM INT; (while true; do sleep 10; done) & wait
+
+[[ -z "${S3UID}" ]] || FUSE_OPTS="$FUSE_OPTS -o uid=${S3UID}"
+[[ -z "${S3GID}" ]] || FUSE_OPTS="$FUSE_OPTS -o gid=${S3GID}"
+
+
+s3fs $FUSE_OPTS $BUCKET $MOUNTPOINT  &&  echo $BUCKET mounted on $MOUNTPOINT...
+trap : TERM INT;  wait
